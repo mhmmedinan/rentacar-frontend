c72d6503deae27d0e0a9bed5b254dc1ce25e67e9
mrm65 <minan.65@hotmail.com>
2021-03-17 02:27:59 +0300

    18.Hafta

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {CarResponseModel} from "../models/carResponseModel";
 import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Car } from '../models/car';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarService {
-  apiUrl="https://localhost:44367/api/cars/getdetail";
+  apiUrl="https://localhost:44367/api/";
   constructor(private httpClient:HttpClient) { }
 
-  getCars():Observable<CarResponseModel>
+  getCars():Observable<ListResponseModel<Car>>
   {
-    return this.httpClient.get<CarResponseModel>(this.apiUrl)
+    let newPath=this.apiUrl+"cars/getdetail"
+    return this.httpClient.get<ListResponseModel<Car>>(newPath)
   }
+
+  getCarsByBrand(brandId:number):Observable<ListResponseModel<Car>>
+  {
+    let newPath = this.apiUrl+"cars/getcarbybrand?brandId="+brandId
+    return this.httpClient.get<ListResponseModel<Car>>(newPath)
+  }
+
+  getCarsByColor(colorId:number):Observable<ListResponseModel<Car>>
+  {
+    let newPath=this.apiUrl+"cars/getcarbycolor?colorId="+colorId
+    return this.httpClient.get<ListResponseModel<Car>>(newPath)
+  }
+  
 }
